681871a039c7004a6d85c282ec07f5468555d8be
prettygood236 <prettygood236@gmail.com>
2023-11-01 19:28:56 +0900

    Storing and iterating data sequences using arrays and foreach statements in C#

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -6,29 +6,13 @@ namespace TestProject
     {
         static void Main(string[] args)
         {
-            Random random = new Random();
-            int daysUntilExpiration = random.Next(12);
-            int discountPercentage = 0;
-
-            Console.WriteLine(daysUntilExpiration);
-            if(daysUntilExpiration == 0)
+            string[] OrderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+            foreach (string OrderID in OrderIDs)
             {
-                Console.WriteLine("Your subscription has expired.");
-            }
-            else if(daysUntilExpiration == 1)
-            {
-                Console.WriteLine("Your subscription expires within a day! Renew now and save 20%!");
-            }
-            else if(daysUntilExpiration <= 5)
-            {
-                Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days. Renew now and save 10%!");
-            }
-            else if(daysUntilExpiration <= 10)
-            {
-                Console.WriteLine("Your subscription will expire soon. Renew now!");
-            }
-            else
-            {
+                if (OrderID.StartsWith("B"))
+                {
+                    Console.WriteLine(OrderID);
+                }
             }
         }
     }
